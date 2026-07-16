55073f8e43f12516f8f19c35d321c7fbc7eeed99
RajkrRabidas <singhkabir05185@gmail.com>
2026-07-16 23:55:40 +0530

    feat: implement CreateShop function with validation and error handling in shop.controller.js

--- Backend/controllers/shop.controller.js
+++ Backend/controllers/shop.controller.js
@@ -1,4 +1,5 @@
 const cloudinary = require("cloudinary")
+const shopModel = require("../models/shop.model")
 
 const uploadToCloudinary = async (req, res) => {
     try{
@@ -12,4 +13,38 @@ const uploadToCloudinary = async (req, res) => {
     }catch(error){
         res.status(500).json({ message: "Internal Server Error" });
     }
+}
+
+const CreateShop  = async (req, res) => {
+    const user = req.user;
+
+    if(!user){
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const existingShop = await shopModel.findOne({ ownerId: user._id });
+
+    if(existingShop){
+        return res.status(400).json({ message: "Shop already exists" });
+    }
+
+    const { name, description, image, phone, latitude, longitude, formatted } = req.body;
+
+    if(!name || !latitude || !longitude) {
+        return res.status(400).json({ message: "Please give all details" });
+    }
+
+    const file = req.file
+
+    if(!file){
+        return res.status(400).json({message: "please give image"})
+    }
+
+    const fileBuffer = getBuffer(file)
+
+    if(!fileBuffer){
+        return res.status(500).json({message:"Filed to Create file Buffer"})
+    }
+
+    
 }
